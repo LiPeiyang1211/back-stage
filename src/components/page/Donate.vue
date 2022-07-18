f890170cd6f42bfcf8666e693b16d7b21cb6df81
℡啨天丶 <11348757+lpy1211@user.noreply.gitee.com>
2022-07-18 16:20:13 +0800

    ‘............’

---
+++ src/components/page/Donate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-settings"></i> 联系维护
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">微信号：LiPeiyang0_0</div>
+            <div>
+                <img src="https://lipeiyang-test-1311381084.cos.ap-nanjing.myqcloud.com/weixin.jpg" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
